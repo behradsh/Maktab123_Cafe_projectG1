7c365521b80465feaf0d59dc15bcc55d0912c27f
siavash <siavashshoar2@gmail.com>
2025-02-19 17:11:35 +0330

    some changes in Customer model applied.

--- myproject/customer/admin.py
+++ myproject/customer/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Customers, Message
 # Register your models here.
-admin.site.register(Customers)
-admin.site.register(Message)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'phone_number', 'role')
+    search_fields = ['first_name', 'last_name', 'email']
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('description', 'user')
+    search_fields = ['description']
+    list_filter = ['user']
+    
+    
+admin.site.register(Customers, CustomerAdmin)
+admin.site.register(Message, MessageAdmin)
--- myproject/customer/models.py
+++ myproject/customer/models.py
@@ -10,11 +10,23 @@ class Customers(AbstractUser):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100, blank= True)
     phone_number = PhoneNumberField()
-    email = models.EmailField()
+    email = models.EmailField(unique= True)
     password = models.CharField(max_length=128)
     birth_date = models.DateField(null = True, blank= True)
     role = models.CharField(max_length= 10, choices= ROLE_CHOICES, default= 'customer' )
-    
+    username = models.CharField(max_length=150, unique= True, default= 'default_username')
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name= 'customer_groups',
+        blank= True,
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name = 'customers_user_permissions',
+        blank= True,
+    )
 class Message(models.Model):
     description = models.TextField()
     user = models.ForeignKey(Customers, on_delete= models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
