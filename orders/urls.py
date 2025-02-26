e28a7c2b83cb2170d2049a7e4973c2f171adfbc2
Seyyed-Ali99 <hoseinishakor@gmail.com>
2025-02-27 02:17:41 +0330

    urls file created for orders

---
+++ orders/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("order_detail/",views.OrderDetail.as_view(),name="order_detail"),
+    path("general_order/",views.OrderDetail.as_view(),name="order"),
+    path("receipt/",views.Receipt.as_view(),name="receipt"),
+    path("table/",views.Table.as_view(),name="table"),
+]
